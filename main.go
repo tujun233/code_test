d25f810f3574b26ed2f2efad3aef7a46d5a27b63
weizhimin <weizhimin.001@bytedance.com>
2025-05-20 22:11:59 +0800

    first

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Age int64 `json:"age,omitempty"`
+	A   *User `json:"a"`
+}
+
+type User struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	var p []int
+	fmt.Println(p)
+	change(p)
+	fmt.Println(p)
+}
+
+func change(p []int) {
+	p = append(p, 1)
+}
